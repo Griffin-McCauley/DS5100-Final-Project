7dd202be0b77dfaec363f3fb6cd0ea59f68b23a4
Griffin McCauley <kzj5qw@virginia.edu>
2022-07-12 09:12:51 -0400

    Added directory structure

---
+++ montecarlo/montecarlo.py
@@ -0,0 +1,125 @@
+import numpy as np
+import pandas as pd
+
+class Die():
+    '''This Die class defines an object which has N sides, or “faces”, and W weights, and can be rolled to select a face.'''
+    
+    def __init__(self, faces):
+        '''
+        This initializer takes an array of faces (strings or numbers) as an input and defines a uniform distribution over the sample space of faces, storing this distribution as a private dataframe.
+        '''
+        if len(set(map(type, faces))) == 1:
+            self.faces = np.array(faces)
+            self.weights = np.ones(len(faces))
+            self._die = pd.DataFrame({'faces':self.faces, 'weights':self.weights})
+        else:
+            return print('Please input face values of the same data type.')
+        
+    def change_weight(self, face, new_weight):
+        '''
+        Purpose: to change the weight of a single side
+        Inputs: 
+            - the face value to be changed (string or number depending on initialization)
+            - the new weight (float)
+        Output: a modified weights distribution
+        '''
+        if face not in self.faces:
+            return print('Please input a valid face value.')
+        try:
+            new_weight = float(new_weight)
+        except ValueError:
+            return print('Please input a valid weight value (i.e. a float).')       
+        self.weights[self.faces == face] = new_weight
+        self._die['weights'] = self.weights
+                
+    def roll(self, n = 1):
+        '''
+        Purpose: to roll the die one or more times
+        Input: a number to specify how many times the dice should be rolled
+        Output: a list of outcomes
+        '''
+        try:
+            n = round(float(n))
+        except ValueError:
+            return print('Please input a valid number of rolls (i.e. an integer).')
+        probs = [i/sum(self.weights) for i in self.weights]
+        return [self._die.faces.sample(weights=probs).values[0] for roll in range(n)]
+            
+    def show_die(self):
+        '''This method shows the die’s current set of faces and weights.'''
+        return self._die
+    
+    
+    
+class Game():
+    '''This Game class defines an object which consists of rolling of one or more dice of the same kind one or more times. '''
+    
+    def __init__(self, dice):
+        '''This initializer takes a list of already instantiated similar Die objects as its single input parameter.'''
+        # check about whether we need to check that dice are similar or not...
+        self.dice = dice
+        
+    def play(self, n):
+        '''
+        Purpose: to play the game by rolling the dice a specified number of times
+        Input: a number to specify how many times the dice should be rolled
+        Output: a private dataframe of shape N rolls by M dice with each entry indicating the face rolled in that instance
+        '''
+        try:
+            n = round(float(n))
+        except ValueError:
+            return print('Please input a valid number of rolls (i.e. an integer).')
+        results = pd.DataFrame()
+        for i in range(len(self.dice)):
+            results = pd.concat([results, pd.DataFrame({i:self.dice[i].roll(n)})], axis=1)
+        results.index = [i+1 for i in range(n)]
+        results.index.name = 'roll_number'
+        results.columns.name = 'die_number'
+        self._results = results
+
+    def show_result(self, form = 'wide'):
+        '''
+        Purpose: to pass the private dataframe of results to the user
+        Input: a form parameter ('wide' or 'narrow'), specifying whether to return the dataframe in 'narrow' or 'wide' form
+        Output: the private dataframe of results
+        '''
+        if form == 'wide':
+            return self._results
+        elif form == 'narrow':
+            return self._results.stack().unstack(level=0).stack().to_frame('face_rolled')
+        elif form not in ['wide','narrow']:
+            raise Exception("The form parameter must be 'wide' or 'narrow'.")
+    
+    
+    
+class Analyzer():
+    '''This Analyzer class defines an object which takes the results of a single game and computes various descriptive statistical properties about it.'''
+
+    def __init__(self, game):
+        '''This initializer takes a game object as its single input parameter.'''
+        self.game = game
+        
+    def jackpot(self):
+        '''This method computes how many times the game resulted in all faces being identical.'''
+        jackpot_df = pd.DataFrame()
+        for i in range(len(self.game._results)):
+            jackpot_df = pd.concat([jackpot_df, pd.DataFrame({'jackpot': [(len(set(self.game._results.iloc[i])) == 1)]})], axis = 0)
+        jackpot_df.index = [i+1 for i in range(len(self.game._results))]
+        jackpot_df.index.name = 'roll_number'
+        self.jackpots = jackpot_df
+        return int(sum(self.jackpots['jackpot']))
+        # return int(sum([(len(set(self.game._results.iloc[i])) == 1) for i in range(len(self.game._results))]))
+    
+    def combo(self):
+        '''This method computes the distinct combinations of faces rolled, along with their counts.'''
+        rolls = [sorted(list(self.game._results.iloc[i])) for i in range(len(self.game._results))]
+        self.combos = pd.DataFrame(rolls).value_counts().to_frame('counts')
+        
+    def face_counts_per_roll(self):
+        '''This method computes how many times a given face is rolled in each event.'''
+        fcpr = pd.DataFrame()
+        for i in range(len(self.game._results)):
+            fcpr = pd.concat([fcpr, pd.DataFrame({'Ace': [sum(list(self.game._results.iloc[i] == 'Ace'))], 'King': [sum(list(self.game._results.iloc[i] == 'King'))], 'Queen': [sum(list(self.game._results.iloc[i] == 'Queen'))], 'Jack': [sum(list(self.game._results.iloc[i] == 'Jack'))]})], axis = 0)
+        fcpr.index = [i+1 for i in range(len(self.game._results))]
+        fcpr.index.name = 'roll_number'
+        self.face_counts_per_roll = fcpr
---
+++ montecarlo/tests/montecarlo_tests.py
@@ -0,0 +1,125 @@
+import numpy as np
+import pandas as pd
+import unittest
+from ../montecarlo import Die, Game, Analyzer
+
+class DieGameTestSuite(unittest.TestCase):
+    '''This DieGameTestSuite class contains the test suite of methods used to unit test the Die, Game, and Analyzer classes.'''
+    
+    def test_001_init(self):
+        die = Die(['H', 'T'])
+        self.assertTrue(all(np.array(die._die['faces']) == np.array(['H','T'])) and all(np.array(die._die['weights']) == np.ones(len(['H','T']))))
+        
+    def test_002_change_weight_success(self):
+        die = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die.change_weight('Ace', 10)
+        self.assertTrue(die.weights[die.faces == 'Ace'] == 10 and all(weight == 1 for weight in die.weights[die.faces != 'Ace']))
+        
+    def test_003_change_weight_failure(self):
+        die = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die.change_weight('Ace', 'ten')
+        self.assertTrue(die.weights[die.faces == 'Ace'] == 1)
+        
+    def test_004_roll_success(self):
+        die = Die(['H', 'T'])
+        outcomes = die.roll(10)
+        self.assertTrue(len(outcomes) == 10 and all(outcome in ['H', 'T'] for outcome in outcomes))
+        
+    def test_005_roll_failure(self):
+        die = Die(['H', 'T'])
+        outcomes = die.roll('ten')
+        self.assertTrue(outcomes == None)
+        
+    def test_006_show_die(self):
+        die = Die(['H', 'T'])
+        shown = die.show_die()
+        self.assertTrue(all(np.array(shown['faces']) == np.array(['H','T'])) and all(np.array(shown['weights']) == np.ones(len(['H','T']))))
+        
+        
+        
+    def test_010_init(self):
+        die1 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die2 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die3 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        game = Game([die1, die2, die3])
+        self.assertTrue(all(type(die) == Die for die in game.dice))
+        
+    def test_020_play_success(self):
+        die1 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die2 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die3 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        game = Game([die1, die2, die3])
+        game.play(10)
+        self.assertTrue(game._results.shape == (10, 3) and game._results.index.names[0] == 'roll_number' and game._results.columns.names[0] == 'die_number')
+                
+    def test_030_play_failure(self):
+        die1 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die2 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die3 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        game = Game([die1, die2, die3])
+        game.play('ten')
+        self.assertFalse(hasattr(game, '_results'))
+        
+    def test_040_show_result_narrow(self):
+        die1 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die2 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die3 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        game = Game([die1, die2, die3])
+        game.play(10)
+        result = game.show_result('narrow')
+        self.assertTrue(result.shape == (30, 1) and result.index.names == ['die_number', 'roll_number'] and result.columns[0] == 'face_rolled')
+        
+    def test_050_show_result_failure(self):
+        die1 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die2 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die3 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        game = Game([die1, die2, die3])
+        game.play(10)
+        with self.assertRaises(Exception):
+            game.show_result('standard')
+            
+            
+            
+    def test_100_init(self):
+        die1 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die2 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die3 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        game = Game([die1, die2, die3])
+        game.play(10)
+        analyzer = Analyzer(game)
+        self.assertTrue(analyzer.game._results.shape == (10, 3))
+        
+    def test_200_jackpot(self):
+        die1 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die2 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die3 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        game = Game([die1, die2, die3])
+        game.play(10)
+        analyzer = Analyzer(game)
+        jackpots = analyzer.jackpot()
+        self.assertTrue(type(jackpots) == int and len(analyzer.jackpots) == 10)
+        
+    def test_300_combo(self):
+        die1 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die2 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die3 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        game = Game([die1, die2, die3])
+        game.play(10)
+        analyzer = Analyzer(game)
+        analyzer.combo()
+        self.assertTrue(hasattr(analyzer, 'combos') and analyzer.combos.columns[0] == 'counts' and sum(analyzer.combos['counts']) == 10)
+        
+    def test_400_face_counts_per_roll(self):
+        die1 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die2 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        die3 = Die(['Ace', 'King', 'Queen', 'Jack'])
+        game = Game([die1, die2, die3])
+        game.play(10)
+        analyzer = Analyzer(game)
+        analyzer.face_counts_per_roll()
+        self.assertTrue(hasattr(analyzer, 'face_counts_per_roll') and analyzer.face_counts_per_roll.shape == (10, 4) and all([sum(analyzer.face_counts_per_roll.iloc[i]) == 3 for i in range(len(analyzer.face_counts_per_roll))]))
+           
+            
+            
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
