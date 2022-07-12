a773dd3c9d9bfd64070eb67c2b2b1a8e77c702ea
Griffin McCauley <kzj5qw@virginia.edu>
2022-07-12 09:22:38 -0400

    Added setup.py and __init__.py files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Monte Carlo Simulator',
+    version='1.0.0',
+    description='This package provides the tools to run a simple Monte Carlo simulator.',
+    url='https://github.com/Griffin-McCauley/DS5100-Final-Project',
+    author='Griffin McCauley',
+    author_email='kzj5qw@virginia.edu',
+    license='MIT',
+    packages=find_packages()
+)
